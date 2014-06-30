f42ef5c78ae7be3361c50538cd1435aa101f22d4
Ritwik Dutta <ritzymail@gmail.com>
2014-06-29 17:39:35 -0700

    Stuff.

---
+++ server/query.py
@@ -0,0 +1,65 @@
+from pymongo import MongoClient
+from datetime import timedelta
+from flask import Flask,make_response, request, current_app
+from functools import update_wrapper
+import requests
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+app = Flask(__name__)
+
+db = MongoClient('localhost', 27017).data
+users = db.users
+
+motion = db.motion
+
+@app.route('/getuser=<name>')
+@crossdomain(origin='*')
+def getMoves(access_token):
+    for user in users:
+    	if user['name'] == name:
+    		return user['name']
+    return "Not found."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081, debug=True)
+
+
