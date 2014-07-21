7b3cdca9b9715101e3340820ecd60a86af3fa4e8
Ritwik Dutta <ritzymail@gmail.com>
2014-07-21 15:30:30 -0700

    Fixed stuff.

--- server/sensor.py
+++ server/sensor.py
@@ -1,85 +1,11 @@
-from pymongo import MongoClient
+from time import strftime as gtime
 from datetime import timedelta
-from flask import Flask, make_response, request, current_app
-from functools import update_wrapper
+from random import randrange
+from os import listdir
+import threading
 import requests
 import json
 import nltk
-from random import randrange
-from time import strftime as gtime
-from os import listdir
-
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
-app = Flask(__name__)
-
-db = MongoClient('localhost', 27017).data
-users = db.users
-motion = db.motion
-sleep = db.sleep
-photos = db.photos
-weather = db.weather
-
-@app.errorhandler(404)
-@app.route('/<username>')
-@crossdomain(origin='*')
-def getDate(username):
-    if request.args.get('type') == 'user':
-        for user in users.find():
-            if user['name'] == username:
-                del(user['_id'])
-                return json.dumps(user)
-        return json.dumps({'name' : 'null' }), 404
-    elif request.args.get('type') == 'data':
-        metric = request.args.get('metric')
-        date = request.args.get('date')
-        data = db[metric].find_one({'metric' : metric, 'name' : username, 'date': eval(date)})
-        if (data == None):
-            return json.dumps({'metric' : metric, 'name' : username, 'date' : eval(date), 'value' : 'null'})
-        del(data['_id'])
-        return json.dumps(data)
-    elif request.args.get('type') == 'write':
-        writeData(username, eval(request.args.get('date')))
-        return 'Data written.'
-    else:
-        return 'Error.'
 
 def writeData(username, date):
     if date == eval(gtime('%Y%m%d')):
@@ -163,7 +89,9 @@ def writeSleepData(username, date):
             print("Sleep data inserted.")
         else:
             print("Sleep data already found.")
-        
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081, debug=True)
+def runAutoWrite():
+    threading.Timer(300, runAutoWrite).start()
+    writeData()
+
+runAutoWrite()
