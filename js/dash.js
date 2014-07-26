0090a03b46f3911d91d391a88f4742d70c7aefb1
Ritwik Dutta <ritzymail@gmail.com>
2014-07-26 16:59:37 -0700

    Fixing more file paths.

--- js/dash.js
+++
@@ -1,7 +0,0 @@
-fillDates();
-
-$(".date.item").on('click', function() {
-    console.log("test");
-    $(".date.item").attr("class", "date item");
-    $(this).attr("class", "date item selected");
-});
