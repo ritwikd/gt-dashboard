5a553f0706da534e188a6f514144a273f137ab65
Ritwik Dutta <ritzymail@gmail.com>
2014-07-26 19:22:35 -0700

    Stuff.

--- server/sensor.py
+++ server/sensor.py
@@ -4,12 +4,12 @@ from datetime import datetime
 from os import listdir
 
 
-def writeData(user, date):
+def writeData(user, date, db):
     metrics = user['metrics']
     for metric in metrics:
 	print metrics[metric]
 	if metrics[metric]['source'] == 'file':
 		filePath = metrics[metric]['path'][0] + user['username'] + '/' + metrics[metric]['path'][1]
 		if metrics[metric]['format'] == 'percentage':
-			
-							
+			percentageDataObject = singleNumberLib(filePath)
+			percentageData = percentageDataObject.getFileData()				
