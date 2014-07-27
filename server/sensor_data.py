7dc8071c6195694e274c180e84df958b0f7b17e4
Ritwik Dutta <ritzymail@gmail.com>
2014-07-26 21:23:49 -0700

    Stupid vim.

--- server/sensor_data.py
+++ server/sensor_data.py
@@ -13,41 +13,40 @@ def writeData(user, date, db):
     username = user['username']
     for metric in metrics:
     	dbInsertObject = { "date" : eval(gtime("%Y%m%d")), "username" : username, "metric" : metric}
-		if metrics[metric]['source'] == 'file':
-			filePath = metrics[metric]['path'][0] + username + '/' + metrics[metric]['path'][1]
-			dbMetricCollection = db[metric]
+	if metrics[metric]['source'] == 'file':
+		filePath = metrics[metric]['path'][0] + username + '/' + metrics[metric]['path'][1]
+		dbMetricCollection = db[metric]
+	
+		if metrics[metric]['format'][0] == 'percentage':
+			fileDataObject = standard.singleNumberLib(filePath)
+			fileData = "";
+			fileData = fileDataObject.getFileData()				
+			if fileData == None:
+				fileData = ""
+			dbInsertObject['value'] = eval(fileData)
 
-			if metrics[metric]['format'][0] == 'percentage':
-				fileDataObject = standard.singleNumberLib(filePath)
-				fileData = "";
-				fileData = fileDataObject.getFileData()				
-				if fileData == None:
-					fileData = ""
-				dbInsertObject['value'] = eval(fileData)
+		elif metrics[metric]['format'][0] == 'graph':
+			fileDataObject = standard.multipleNumberLib(filePath)
+			fileData = "";
+			fileData = fileDataObject.getFileData()				
+			if fileData == None:
+				fileData = ""
+			dbInsertObject['value'] = fileData
 
-			elif metrics[metric]['format'][0] == 'graph':
-				fileDataObject = standard.multipleNumberLib(filePath)
-				fileData = "";
-				fileData = fileDataObject.getFileData()				
-				if fileData == None:
-					fileData = ""
-				dbInsertObject['value'] = fileData
+		elif metrics[metric]['format'] == 'raw':
+			fileDataObject = standard.rawFileLib(filePath)
+			fileData = "";
+			fileData = fileDataObject.getFileData()				
+			if fileData == None:
+				fileData = ""
+			dbInsertObject['value'] = fileData
 
-			elif metrics[metric]['format'] == 'raw':
-				fileDataObject = standard.rawFileLib(filePath)
-				fileData = "";
-				fileData = fileDataObject.getFileData()				
-				if fileData == None:
-					fileData = ""
-				dbInsertObject['value'] = fileData
-
-			else metrics[metrics]['format'] == 'picture':
-				fileDataObject = standard.pictureFileLib(filePath)
-				fileData = "";
-				fileData = fileDataObject.getFileData()				
-				if fileData == None:
-					fileData = ""
-				dbInsertObject['value'] = fileData
-
-		dbMetricCollection.insert(dbInsertObject)
+		else metrics[metrics]['format'] == 'picture':
+			fileDataObject = standard.pictureFileLib(filePath)
+			fileData = "";
+			fileData = fileDataObject.getFileData()				
+			if fileData == None:
+				fileData = ""
+			dbInsertObject['value'] = fileData
+	dbMetricCollection.insert(dbInsertObject)
 
