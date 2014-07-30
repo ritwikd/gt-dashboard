8b2941e5c3585689ebac2f6503e3639235621128
Ritwik Dutta <ritzymail@gmail.com>
2014-07-30 14:59:34 -0700

    Working on stuff.

--- server/user.py
+++ server/user.py
@@ -4,18 +4,21 @@ from os import makedirs as makeDirectory
 
 class newUserLib:
 	def __init__(self, newUserUsername, newUserFullname, newUserSleep, newUserMotion, databaseUserCollection):
-		self.newUserUsername = newUserFullname.replace(" ", "") + str(epochTime(epochFirst()))
+		self.newUserUsername = newUserFullname.replace(' ', '') + str(epochTime(epochFirst()))
 		self.newUserTargets = [eval(newUserSleep), eval(newUserMotion)]
 		self.newUserFullname = newUserFullname
 		self.databaseUserCollection = databaseUserCollection
 
 	def createUser(self):
-		userTemplateJSON = { "name" : self.newUserFullname, "username" : self.newUserUsername, "metrics" : { "sleep" : { "order" : 2, "source" : "file", "format" : ["percentage", self.newUserTargets[0], "hours"], "path" : ["mongo/data/db/", "sleep"], }, "weather" : { "order" : 3, "source" : "custom", "format" : ["graph", "&deg; C"], "type" : "weather", "location" : "ksfo", }, "pictures" : { "order" : 4, "source" : "file", "format" : ["picture"], "path" : ["mongo/data/db/", "pictures"], }, "motion" : { "order" : 1, "source" : "custom", "format" : ["percentage", self.newUserTargets[1], "steps"], "type" : "jawbone", "auth" : "b6_3pfGGwEjReOXSnWIyQO0-Al13wvvmyZNaiuNHtPrR6_kAcLtg_W1PaWiav9FR8EvaJSumcI0GoYT-V9UbpVECdgRlo_GULMgGZS0EumxrKbZFiOmnmAPChBPDZ5JP" } } }
+		userTemplateJSON = { 'name' : self.newUserFullname, 'username' : self.newUserUsername, 'metrics' : { 'sleep' : { 'order' : 2, 'source' : 'file', 'format' : ['percentage', self.newUserTargets[0], 'hours'], 'path' : ['mongo/data/db/', 'sleep'], }, 'weather' : { 'order' : 3, 'source' : 'custom', 'format' : ['graph', '&deg; C'], 'type' : 'weather', 'location' : 'ksfo', }, 'pictures' : { 'order' : 4, 'source' : 'file', 'format' : ['picture'], 'path' : ['mongo/data/db/', 'pictures'], }, 'motion' : { 'order' : 1, 'source' : 'custom', 'format' : ['percentage', self.newUserTargets[1], 'steps'], 'type' : 'jawbone', 'auth' : 'b6_3pfGGwEjReOXSnWIyQO0-Al13wvvmyZNaiuNHtPrR6_kAcLtg_W1PaWiav9FR8EvaJSumcI0GoYT-V9UbpVECdgRlo_GULMgGZS0EumxrKbZFiOmnmAPChBPDZ5JP' } } }
 		self.databaseUserCollection.insert(userTemplateJSON)
-		makeDirectory("mongo/data/db/" +  self.newUserUsername + "/")
-		makeDirectory("mongo/data/db/" +  self.newUserUsername + "/pictures")
-		makeDirectory("mongo/data/db/" +  self.newUserUsername + "/sleep")
-		return self.databaseUserCollection.find_one({ "username" : self.newUserUsername})
+		makeDirectory('mongo/data/db/' +  self.newUserUsername + '/')
+		for (metric in userTemplateJSON['metrics']) {
+			tempMetric = userTemplateJSON['metrics'][metric]
+			if (tempMetric['source'] == 'file'):
+				makeDirectory(tempMetric[path][0] +  self.newUserUsername + '/' + tempMetric[path][1])		
+		}
+		return self.databaseUserCollection.find_one({ 'username' : self.newUserUsername})
 
 class deleteUserLib:
 	def __init__(self, deleteUserUsername, dataBaseUserCollection):
@@ -23,7 +26,7 @@ class deleteUserLib:
 		self.dataBaseUserCollection = dataBaseUserCollection
 
 	def deleteUser(self):
-		deleteKey = { "username" : self.deleteUserUsername }
+		deleteKey = { 'username' : self.deleteUserUsername }
 		if (self.dataBaseUserCollection.find_one(deleteKey) != None):
 			self.dataBaseUserCollection.remove(deleteKey)
 
---
+++ wiki/home.md
@@ -0,0 +1,69 @@
+# Overview
+
+Each user in the database has a set of metrics. The metrics are stored in JSON format and, here is an example user object:
+
+```JSON
+{  
+   "name":"Ritwik Dutta",
+   "username":"ritwikdutta1406421674",
+   "metrics":{  
+      "sleep":{  
+         "source":"file",
+         "format":[  
+            "percentage",
+            8,
+            "hours"
+         ],
+         "path":[  
+            "mongo/data/db/",
+            "sleep"
+         ]
+      },
+      "weather":{  
+         "source":"custom",
+         "format":[  
+            "graph",
+            "° C"
+         ],
+         "type":"weather",
+         "location":"ksfo"
+      },
+      "pictures":{  
+         "source":"file",
+         "format":[  
+            "picture"
+         ],
+         "path":[  
+            "mongo/data/db/",
+            "pictures"
+         ]
+      },
+      "motion":{  
+         "source":"custom",
+         "format":[  
+            "percentage",
+            400,
+            "steps"
+         ],
+         "type":"jawbone",
+         "auth":"b6_3pfGGwEjReOXSnWIyQO0-Al13wvvmyZNaiuNHtPrR6_kAcLtg_W1PaWiav9FR8EvaJSumcI0GoYT-V9UbpVECdgRlo_GULMgGZS0EumxrKbZFiOmnmAPChBPDZ5JP"
+      }
+   }
+}
+```
+
+To add a metric, you have to add it as a key and value in the **"metrics"** object in the user object. 
+
+The metric can have two possible **"sources"** on the backend, which define how the data is collected. Either the metric can have a **"custom"** type, where everything has to be custom-implemented, or the metric can have a **"file"** type, where everything is automatically pulled from a path. The path for a metric is currently defined as "/mongo/data/db/<username\>/<metric\>".
+
+The metric can have four possible **"formats"** on the frontend, which defines how the data is displayed. They are as follows:
+
+ - **Percentage**, where the metric is displayed as a progress bar towards a target
+ - **Picture**, where the metric is displayed as a series of photos
+ - **Graph**, where the metric is displayed as a graph
+ - **Raw**, where the data is displayed as pure raw numbers or text
+
+
+ To add new "file" based metrics, simply create a new entry in the user entry with the selected options for display format .
+
+It is also possible to create custom frontend 
