19d5a36e555081e92fc8e275cecaf2bc2391a46d
Ritwik Dutta <ritzymail@gmail.com>
2014-07-30 15:12:47 -0700

    Fixing stuff.

--- server/user.py
+++ server/user.py
@@ -13,11 +13,10 @@ class newUserLib:
 		userTemplateJSON = { 'name' : self.newUserFullname, 'username' : self.newUserUsername, 'metrics' : { 'sleep' : { 'order' : 2, 'source' : 'file', 'format' : ['percentage', self.newUserTargets[0], 'hours'], 'path' : ['mongo/data/db/', 'sleep'], }, 'weather' : { 'order' : 3, 'source' : 'custom', 'format' : ['graph', '&deg; C'], 'type' : 'weather', 'location' : 'ksfo', }, 'pictures' : { 'order' : 4, 'source' : 'file', 'format' : ['picture'], 'path' : ['mongo/data/db/', 'pictures'], }, 'motion' : { 'order' : 1, 'source' : 'custom', 'format' : ['percentage', self.newUserTargets[1], 'steps'], 'type' : 'jawbone', 'auth' : 'b6_3pfGGwEjReOXSnWIyQO0-Al13wvvmyZNaiuNHtPrR6_kAcLtg_W1PaWiav9FR8EvaJSumcI0GoYT-V9UbpVECdgRlo_GULMgGZS0EumxrKbZFiOmnmAPChBPDZ5JP' } } }
 		self.databaseUserCollection.insert(userTemplateJSON)
 		makeDirectory('mongo/data/db/' +  self.newUserUsername + '/')
-		for (metric in userTemplateJSON['metrics']) {
+		for metric in userTemplateJSON['metrics']:
 			tempMetric = userTemplateJSON['metrics'][metric]
 			if (tempMetric['source'] == 'file'):
-				makeDirectory(tempMetric[path][0] +  self.newUserUsername + '/' + tempMetric[path][1])		
-		}
+				makeDirectory(tempMetric['path'][0] +  self.newUserUsername + '/' + tempMetric['path'][1])		
 		return self.databaseUserCollection.find_one({ 'username' : self.newUserUsername})
 
 class deleteUserLib:
