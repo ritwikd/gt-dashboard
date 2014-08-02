8841613810017ae64d4809bac4f17cc1f2df95f8
Ritwik Dutta <ritzymail@gmail.com>
2014-08-01 17:14:33 -0700

    Adding build file.

---
+++ build.py
@@ -0,0 +1,13 @@
+from subprocess import call
+
+def runCommand(command):
+	call(command, shell=True)
+
+fileHandler = open("VERSION")
+buildVersion = fileHandler.read().rstrip()
+fileHandler.close()
+
+
+runCommand("rm -Rf .git")
+runCommand("rm -Rf wiki")
+runCommand("cd .. && tar -cvzf gt-dashboard." + buildVersion + ".tar.gz gt-dashboard/") 
